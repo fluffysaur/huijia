52ce7de7a7c621650f84f4db43108f7bd90b3c46
fluffysaur <tanyijia@gmail.com>
2020-05-09 01:58:31 +0800

    Tweaked a little

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,4 @@
 $( document ).ready(function() {
-   
+    var gamesCard = document.getElementById("gamescard");
+    gamesCard.addEventListener("click", function(){window.location.href = 'post.html'});
 });
